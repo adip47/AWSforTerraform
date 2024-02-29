d6e31ab7cb2cc0f54985bc77b17cc9ea84e9ce05
ADITYA  PAWAR <64777945+adip47@users.noreply.github.com>
2024-02-29 14:45:08 +0530

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -1,12 +1,22 @@
 resource "aws_s3_bucket" "test-bucket-sm" {
         bucket = "test-bucket-sm-${var.env_name}"
         acl = "private"
+        server_side_encryption_configuration {
+         rule {
+          apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+      }
+    }
+}
 }
 resource "aws_s3_bucket" "test-bucket-sm-bkt" {
         bucket = "test-bucket-sm-bkt-${var.env_name}"
         acl = "private"
-}
-resource "aws_s3_bucket" "test-bucket-sm-bkt-test" {
-        bucket = "test-bucket-sm-bkt--test-${var.env_name}"
-        acl = "private"
+        server_side_encryption_configuration {
+         rule {
+          apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+      }
+    }
+  }
 }
