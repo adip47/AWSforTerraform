5df5d2f3b21b95eaac8a68f1bafcb55fc47ce07f
ADITYA  PAWAR <64777945+adip47@users.noreply.github.com>
2024-03-14 10:03:41 +0530

    Update variables.tf

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -20,22 +20,3 @@ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 variable "env_name" {
 default  = "dev"
 }
-variable "public_subnet_cidrs" {
-description = "Public Subnet CIDR values"
-default     = "10.0.1.0/24"
-}
- 
-variable "private_subnet_cidrs" {
-description = "Private Subnet CIDR values"
-default     = "10.0.4.0/24"
-}
- 
- 
-variable "azs" {
-description = "Availability Zones"
-default     = "us-east-1a"
-}
- 
-variable "env_name" {
-default  = "dev"
-}
